34da0d35e1d69869c2618db2639eed321573c5cf
Anton <Shershnewww@mail.ru>
2021-02-17 10:48:57 +0300

    HW5

---
+++ HW5-5.py
@@ -0,0 +1,15 @@
+def summary():
+    try:
+        with open("Test_file_5.txt", "w+") as file_obj:
+            line = input("Введите цифры через пробел \n")
+            file_obj.writelines(line)
+            my_numb = line.split()
+
+            print(sum(map(int, my_numb)))
+    except IOError:
+        print("Ошибка в файле")
+    except ValueError:
+        print("Параметр введён неверно")
+
+
+summary()
